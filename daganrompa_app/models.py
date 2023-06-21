73ed0a649cc9b24621917c438d363f297c2eccb1
7SkyI <lolpuw123@gmail.com>
2023-06-21 16:30:50 +0300

    123

--- daganrompa_app/models.py
+++ daganrompa_app/models.py
@@ -1,29 +1,3 @@
 from django.db import models
 
-
-class UsersModel(models.Model):
-    BAN = (
-        ('T', 'True'),
-        ('F', 'False')
-    )
-    VIP = (
-        ('T', 'True'),
-        ('F', 'False')
-    )
-    name = models.CharField(max_length=16)
-    email = models.EmailField(null=True)
-    discord = models.CharField(max_length=30)
-    steam = models.URLField()
-    total_game = models.IntegerField()
-    history_game = models.IntegerField()
-    # main_hero
-    ban = models.CharField(max_length=1, choices=BAN)
-    vip = models.CharField(max_length=1, choices=VIP)
-
-
-class HeroListModel(models.Model):
-    pass
-
-
-class GameModel(models.Model):
-    pass
+# Create your models here.
