bd0d6da42daee6401cc501486dcc62a2c60d21fe
7SkyI <lolpuw123@gmail.com>
2023-06-21 17:09:28 +0300

    urls

--- daganrompa_app/admin.py
+++ daganrompa_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import UsersModel, HeroListModel, GameModel
 
-# Register your models here.
+
+admin.site.register(UsersModel)
+admin.site.register(HeroListModel)
+admin.site.register(GameModel)
+
+@admin.register(UsersModel)
+class UsersModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(HeroListModel)
+class HeroListModelAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GameModel)
+class GameModelAdmin(admin.ModelAdmin):
+    pass
