0e7bf0d3391a30c97f841d0b06a422207731ddf5
daganrompa <programmawot@gmail.com>
2023-06-21 16:15:40 +0300

    Add file

--- daganrompa_app/models.py
+++ daganrompa_app/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 
+
 class UsersModel(models.Model):
     BAN = (
         ('T', 'True'),
         ('F', 'False')
     )
-    VIP= (
+    VIP = (
         ('T', 'True'),
         ('F', 'False')
     )
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=16)
     email = models.EmailField(null=True)
     discord = models.CharField(max_length=30)
     steam = models.URLField()
     total_game = models.IntegerField()
     history_game = models.IntegerField()
-    #main_hero
-    #hero_list
+    # main_hero
     ban = models.CharField(max_length=1, choices=BAN)
     vip = models.CharField(max_length=1, choices=VIP)
+
+
+class HeroListModel(models.Model):
+    pass
+
+
+class GameModel(models.Model):
+    pass
