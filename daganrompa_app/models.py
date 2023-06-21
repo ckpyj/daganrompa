6faffa3deb1a25e10cbf1680e33bb884631e3213
daganrompa <programmawot@gmail.com>
2023-06-21 13:25:48 +0300

    Add file

--- daganrompa_app/models.py
+++ daganrompa_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class UsersModel(models.Model):
+    BAN = (
+        ('T', 'True'),
+        ('F', 'False')
+    )
+    VIP= (
+        ('T', 'True'),
+        ('F', 'False')
+    )
+    name = models.CharField(max_length=30)
+    email = models.EmailField(null=True)
+    discord = models.CharField(max_length=30)
+    steam = models.URLField()
+    total_game = models.IntegerField()
+    history_game = models.IntegerField()
+    #main_hero
+    #hero_list
+    ban = models.CharField(max_length=1, choices=BAN)
+    vip = models.CharField(max_length=1, choices=VIP)
